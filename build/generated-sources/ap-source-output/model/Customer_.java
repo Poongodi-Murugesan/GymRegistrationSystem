5eb3450f129dc25c610f2cfb725db7926a3aedf4
Poongodi Murugesan <38096458+Poongodi-Murugesan@users.noreply.github.com>
2018-08-23 09:55:54 +0200

    First version

---
+++ build/generated-sources/ap-source-output/model/Customer_.java
@@ -0,0 +1,21 @@
+package model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-08-23T09:42:54")
+@StaticMetamodel(Customer.class)
+public class Customer_ { 
+
+    public static volatile SingularAttribute<Customer, String> membershiptype;
+    public static volatile SingularAttribute<Customer, String> firstName;
+    public static volatile SingularAttribute<Customer, String> lastName;
+    public static volatile SingularAttribute<Customer, String> address;
+    public static volatile SingularAttribute<Customer, Integer> phoneNumber;
+    public static volatile SingularAttribute<Customer, String> id;
+    public static volatile SingularAttribute<Customer, String> personalNumber;
+    public static volatile SingularAttribute<Customer, String> branch;
+    public static volatile SingularAttribute<Customer, String> email;
+
+}
